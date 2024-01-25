16d3ddc3675a7b7b096a6d3722d458cf5b0b98b4
Agon Hamiti <98095419+agonhamiti@users.noreply.github.com>
2024-01-25 23:35:45 +0100

    Update ThesisRepository.cs

--- Data/ThesisRepository/ThesisRepository.cs
+++ Data/ThesisRepository/ThesisRepository.cs
@@ -19,13 +19,13 @@ namespace DiplomaThesisDigitalization.Data.ThesisRepository
          // Get all entities of type TEntity
         public IQueryable<TEntity> GetAll()
         {
-            return _dbContext.Set<TEntity>();
+            return  _dbContext.Set<TEntity>();  
         }
 
         
         public IQueryable<TEntity> GetById(Expression<Func<TEntity, bool>> expression)
         {
-            return _dbContext.Set<TEntity>().Where(expression);
+            return  _dbContext.Set<TEntity>().Where(expression);
         }
 
         public void Create(TEntity entity)
@@ -34,19 +34,19 @@ namespace DiplomaThesisDigitalization.Data.ThesisRepository
         }
         public async Task CreateAsync(TEntity entity)
         {
-            await _dbContext.Set<TEntity>().AddAsync(entity);
+            await  _dbContext.Set<TEntity>().AddAsync(entity);
         }
         
         // Delete an existing entity
         public void Delete(TEntity entity)
         {
-            _dbContext.Set<TEntity>().Remove(entity);
+             _dbContext.Set<TEntity>().Remove(entity);
         }
         
         // Update an existing entity
         public void Update(TEntity entity)
         {
-            _dbContext.Set<TEntity>().Update(entity);
+             _dbContext.Set<TEntity>().Update(entity);
         }
         
     }
