087d2569ca12bb29e5663828c8a01c45a6295b97
DardanKabashi <93317606+DardanKabashi@users.noreply.github.com>
2024-01-29 00:47:36 +0100

    Update ThesisDbContext.cs

--- Data/ThesisDbContext.cs
+++ Data/ThesisDbContext.cs
@@ -3,12 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DiplomaThesisDigitalization.Data
 {
+    // Definimi i klasës për ThesisDbContext, e cila trashëgon nga DbContext
     public class ThesisDbContext : DbContext
     {
+        // Konstruktori që merr DbContextOptions si parameter dhe e kalon atë te konstruktori i klasës bazë
         public ThesisDbContext(DbContextOptions<ThesisDbContext> options) : base(options)
         {
             
         }
+
+        //  DBSet "properties" që paraqesin tabelat në bazën e të dhënave 
         public DbSet<User> Users { get; set; }
         public DbSet<Student> Students { get; set; }
         public DbSet<Mentor> Mentors { get; set; }
@@ -20,9 +24,10 @@ namespace DiplomaThesisDigitalization.Data
         public DbSet<Department> Departments { get; set; }
         public DbSet<ConsultationSchedule> ConsultationSchedules { get; set; }
 
+        // Metoda për konfigurimin e lidhjeve dhe kufizimeve midis entiteteve në bazën e të dhënave
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            // Përcakto një lidhje një-me-një mes 'Student' dhe 'DiplomaThesis', me kufizime në "foreign key"
             modelBuilder.Entity<Student>()
             .HasOne<DiplomaThesis>(s => s.DiplomaThesis)
             .WithOne(dt => dt.Student)
